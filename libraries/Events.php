c395235cb649ac440f57dc95cc3a370891938a46
Kristian Matthews <kristian.matthews@me.com>
2016-01-15 22:17:29 +0000

    Updated Events

--- libraries/Events.php
+++ libraries/Events.php
@@ -2,55 +2,62 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Events library
+ * Events library.
  */
 class Events {
 
 	/**
-	 * List of all events
+	 * @var CI_Controller CodeIgniter instance.
 	 */
-	private $events = array();
-
 	protected $CI;
 
 	/**
-	 * Constructor
+	 * @var array Events.
+	 */
+	private $events = array();
+
+	/**
+	 * @var array Objects.
+	 */
+	private $objects = array();
+
+	/**
+	 * @var bool In progress.
+	 */
+	private $in_progress = FALSE;
+
+	/**
+	 * Events constructor.
 	 */
 	public function __construct()
 	{
 		$this->CI =& get_instance();
 
-		log_message('debug', 'Events Library Initialized');
+		foreach ($this->CI->load->get_package_paths() as $package_path)
+		{
+			if (file_exists($package_path . 'config/events.php')) include($package_path . 'config/events.php');
+
+			if (file_exists($package_path . 'config/' . ENVIRONMENT . '/events.php')) include($package_path . 'config/' . ENVIRONMENT . '/events.php');
+		}
+
+		if (file_exists(APPPATH . 'config/events.php')) include(APPPATH . 'config/events.php');
+
+		if (file_exists(APPPATH . 'config/' . ENVIRONMENT . '/events.php')) include(APPPATH . 'config/' . ENVIRONMENT . '/events.php');
+
+		if ( ! isset($event) || ! is_array($event)) return;
+
+		$this->events =& $event;
+
+		log_message('info', 'Events Library Initialized');
 	}
 
 	/**
-	 * Setter
-	 */
-	public function __set($name, $value)
-	{
-		if ( ! isset($this->events[$name])) $this->events[$name] = array();
-
-		array_push($this->events[$name], $value);
-	}
-
-	/**
-	 * Is setter
-	 */
-	public function __isset($name)
-	{
-		return isset($this->events[$name]);
-	}
-
-	/**
-	 * Unsetter
-	 */
-	public function __unset($name)
-	{
-		unset($this->events[$name]);
-	}
-
-	/**
-	 * Calls a particular event
+	 * Calls a particular event.
+	 *
+	 * @param null  $event   Event name.
+	 * @param array ...$data Event data.
+	 *
+	 * @return mixed
 	 */
 	public function call_event($event = NULL, ...$data)
 	{
@@ -58,19 +65,67 @@ class Events {
 
 		if ( ! isset($this->events[$event])) return;
 
-		foreach ($this->events[$event] as $event)
+		foreach ($this->events[$event] as $event_item)
 		{
-			$result = $this->run_event($event, $data);
+			$run_result = $this->run_event($event_item, ...$data);
+
+			if ( ! is_null($run_result)) array_push($result, $run_result);
 		}
 
 		return $result;
 	}
 
 	/**
-	 * Runs a particular event
+	 * Runs a particular event.
+	 *
+	 * @param string $event Event name.
+	 * @param array  ...$data
+	 *
+	 * @return mixed
 	 */
-	private function run_event($event, $data)
+	private function run_event($event, ...$data)
 	{
-		if (is_callable($event)) return $event(...$data);
+		if ($this->in_progress) return;
+
+		if ( ! isset($event['filepath'], $event['filename'])) return;
+
+		$filepath = APPPATH . $event['filepath'] . '/' . $event['filename'];
+
+		if ( ! file_exists($filepath)) return;
+
+		$this->in_progress = TRUE;
+
+		if (isset($this->objects[$events['class']]))
+		{
+			if (method_exists($this->objects[$events['class']], $events['function']))
+			{
+				$result = $this->objects[$events['class']]->$events['function'](...$data);
+			}
+			else
+			{
+				$this->in_progress = FALSE;
+
+				return;
+			}
+		}
+		else
+		{
+			if ( ! class_exists($event['class'], FALSE)) require_once($filepath);
+
+			if ( ! class_exists($event['class'], FALSE) || method_exists($event['class'], $event['function']))
+			{
+				$this->in_progress = FALSE;
+
+				return;
+			}
+
+			$this->objects[$event['class']] = new $event['class']();
+
+			$result = $this->objects[$event['class']]->$event['function'](...$data);
+		}
+
+		$this->in_progress = FALSE;
+
+		return $result;
 	}
 }
