cb14ca388fbb622ee09298c317f0a7249a8f76b8
Kristian Matthews <kristian.matthews@me.com>
2016-02-11 23:08:04 +0000

    Added Copyright

--- helpers/exec_helper.php
+++ helpers/exec_helper.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © 2015 - 2016 Kristian Matthews. All rights reserved.
+ */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 if ( ! function_exists('exec_bg'))
