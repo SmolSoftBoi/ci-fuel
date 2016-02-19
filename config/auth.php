44f39f4e4f5646f6340c95e85a30e6e425851e6a
Kristian Matthews <kristian.matthews@me.com>
2016-02-19 21:14:16 +0000

    Deleted Configs
    Deleted configuration files to allow the user to override them and update the package easily.

--- config/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © 2015 - 2016 Kristian Matthews. All rights reserved.
- */
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['session_prefix'] = '';
